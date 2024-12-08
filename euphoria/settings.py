dc0ae53b4f011a44cc97ffc50a3726077da3183d
Aswin1819 <aswinachumathra@gmail.com>
2024-12-08 16:53:49 +0530

    after including whitenoise app and middleware

--- euphoria/settings.py
+++ euphoria/settings.py
@@ -20,6 +20,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'adminapp.apps.AdminappConfig',  
+    'whitenoise.runserver_nostatic',
     'userapp',
     # Required for Google authentication
     'django.contrib.sites',
@@ -35,6 +36,7 @@ AUTH_USER_MODEL = 'adminapp.EuphoUser'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
